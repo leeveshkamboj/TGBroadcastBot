0215b694dbbe3c3b37e9161d3232e54fb3f231e5
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2021-07-12 12:38:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,11 @@ class Config(object):
     token = os.environ.get("TOKEN", None)
     api_id = os.environ.get("API_ID", None)
     api_hash = os.environ.get("API_HASH", None)
-    ownerID = int(os.environ.get("OWNER_ID", 0))
     log_id = int(os.environ.get("LOG_ID", 0))
     start_msg = os.environ.get("START_MSG", None)
+    ownerID = os.environ.get("OWNER_ID", [])
+    if ownerID:
+        try:
+            ownerID = map(int, ownerID.split("|"))
+        except:
+            ownerID = []
