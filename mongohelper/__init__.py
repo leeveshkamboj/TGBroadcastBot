1d7258d8b160c21992b38164a06ca7621f45d8b0
Leevesh Kamboj <leeveshk12@gmail.com>
2023-03-06 23:44:38 +0530

    updated app.json

--- mongohelper/__init__.py
+++ mongohelper/__init__.py
@@ -3,6 +3,9 @@ from umongo import Instance
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 DATABASE_URI = os.environ.get("DATABASE_URL", None)
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "broadcast")
 
